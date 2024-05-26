7c55aad19dea8c0b6a425ce42c87a9ce06b77158
Altaf <altafhossain2002.bd@gmail.com>
2024-05-26 10:59:49 +0600

    Q1

---
+++ Q1/CreditCardPayment.java
@@ -0,0 +1,14 @@
+public class CreditCardPayment implements PaymentStrategy {
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("making payment using credit");
+        // do something
+        return true;
+    }
+}
+
+
+
+
+
+
